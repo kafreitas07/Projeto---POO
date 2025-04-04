7f316c727537caf14a067f4c11ca09de2d6b28c8
Kaik Freitas <105749605+kafreitas07@users.noreply.github.com>
2025-04-04 17:19:26 -0300

    Create Computador.java

---
+++ Computador.java
@@ -0,0 +1,32 @@
+public class Computador {
+    String marca;
+    float preco;
+    SistemaOperacional sistema;
+    MemoriaUSB memoria;
+    HardwareBasico[] hardware;
+
+    public Computador() {
+
+        sistema = new SistemaOperacional();
+        hardware = new HardwareBasico[3];
+
+    }
+
+    public void MostrarConfig() {
+        System.out.println("Marca: " + marca);
+        for (int i = 0; i < hardware.length ; i++) {
+            if (hardware[i] != null)
+                System.out.println(hardware[i].nome + hardware[i].capacidade);
+        }
+        System.out.println("Sistema Operacional: " + sistema.nome + " - " + sistema.tipo);
+        if (memoria != null) {
+            System.out.println("Memória USB: " + memoria.nome + " - " + memoria.capacidade + " GB");
+        }
+        System.out.println("Preço: R$ " + preco);
+    }
+
+    public void addMemoriaUSB(MemoriaUSB musb) {
+        this.memoria = musb;
+
+    }
+}
